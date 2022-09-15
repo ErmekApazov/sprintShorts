2be58a2cd8e4939191737a7231d35e2ac7fd9f35
Ermek Apazov <apazovermek@gmail.com>
2022-09-15 10:33:18 -0400

    @Configuration

--- src/main/java/com/cydeo/stereotype_annotation/configSession.java
+++ src/main/java/com/cydeo/stereotype_annotation/configSession.java
@@ -1,9 +1,10 @@
 package com.cydeo.stereotype_annotation;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 //@ComponentScan(basePackages = "com.cydeo") // if the package is provided, container will scan basePackages accordingly
 @ComponentScan // without basePackage - container will scan "stereotype_annotation" package only by default.
 public class configSession {
